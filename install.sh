e492278e59dfe5b2bd1c0595ff79ae7a2f073864
muhaeb <muhaeb@gmail.com>
2017-10-26 14:38:31 +0300

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
+(
 cd torch
 ./install-deps
 ./install.sh
 . /root/torch/install/bin/torch-activate
 luarocks install csvigo
 luarocks install dp
+)
 
 #GloVe embeddings
-wget -O glove/glove.840B.300d.zip http://nlp.stanford.edu/data/glove.840B.300d.zip
+(
 cd glove
-unzip glove.840B.300d.zip
-
+glove=glove.840B.300d.zip
+wget http://nlp.stanford.edu/data/$glove
+unzip $glove
+rm -f $glove
+)
