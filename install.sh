571cef0a59db8df64be5c625451062296ead5520
Muhaeb <muhaeb@gmail.com>
2018-01-22 17:55:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 (
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+sudo apt-get update
+sudo apt-get install mono-devel
+)
+
+(
 git clone https://github.com/torch/distro.git ~/torch --recursive
 cd ~/torch; bash install-deps;
 ./install.sh
